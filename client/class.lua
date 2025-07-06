69371ee7022e66f54de5ef9476191066e1a3634f
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-06 19:17:16 +0200

    feat(server): initial TextObject class & factory

---
+++ client/class.lua
@@ -0,0 +1,45 @@
+local TextObject = {}
+TextObject.__index = TextObject
+
+Objects = {}
+
+setmetatable(Objects, {
+	__call = function(self, id)
+		return Objects[id]
+	end
+})
+
+function Objects.new(id, data)
+    local self = {
+        id = id,
+        text = data.text,
+        coords = data.coords,
+        bucket = data?.bucket or 0,
+        expiry = data?.expiry or false,
+        zone = data.zone,
+    }
+
+    local instance = setmetatable(self, TextObject)
+
+    Objects[id] = instance
+    return instance
+end
+
+function Objects.remove(id)
+    Objects[id] = nil
+end
+
+function TextObject:save(transactionId)
+    if self.expiry or not transactionId then return end
+
+    local data = {
+        id = self.id,
+        text = self.text,
+        coords = self.coords,
+        bucket = self.bucket,
+        expiry = false,
+        zone = self.zone,
+    }
+
+    KVP:addToTransaction(transactionId, self.id, data)
+end
