572d4b33ae92f771ea93416a2ecf7127d6b6898a
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-08 00:43:19 +0200

    feat(server/class): added luadoc for SV_TextObject

--- server/class.lua
+++ server/class.lua
@@ -1,3 +1,11 @@
+---@class SV_TextObject
+---@field id string
+---@field text string
+---@field expiry number
+---@field distance number
+---@field coords vector3
+---@field bucket integer
+---@field zone string 
 local TextObject = {}
 TextObject.__index = TextObject
 
@@ -31,6 +39,19 @@ function Objects.remove(id) ---@diagnostic disable-line duplicate-set-field
     Objects[id] = nil
 
     TriggerClientEvent('text-placer:remove-text', -1, id)
+end
+
+---Cycle through all items and return a list of ids of the text objects
+---@param func fun(obj: CL_TextObject): boolean
+---@return table
+function Objects.cycle(func) ---@diagnostic disable-line duplicate-set-field
+    local objs = {}
+    for _, v in pairs(Objects) do
+        if func(v) then
+            objs[#objs+1] = v
+        end
+    end
+    return objs
 end
 
 function TextObject:save(transactionId)
