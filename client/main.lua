22dee74bf8deca9d2b223faef03393f1e5d45ffb
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-08 00:31:16 +0200

    feat(client/main): main threads to update displayable objects & render them

---
+++ client/main.lua
@@ -0,0 +1,31 @@
+while not NetworkIsSessionStarted() do
+    Wait(500)
+end
+
+local displayableObjects = {}
+
+CreateThread(function ()
+    while true do
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+        displayableObjects = Objects.cycle(function (obj)
+            return obj:shouldRender(coords)
+        end)
+        Wait(1000)
+    end
+end)
+
+CreateThread(function ()
+    while true do
+        if #displayableObjects == 0 then
+            Wait(500)
+        else
+            Wait(1)
+        end
+
+        for i = 1, #displayableObjects do
+            local obj = displayableObjects[i]
+            obj:render()
+        end
+    end
+end)
