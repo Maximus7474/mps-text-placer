d6c69d5c0e653769bac2d61fbb42b056d6aff5e7
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-08 12:46:39 +0200

    refactor(client/main): optimisation & behaviour with recent changes

--- client/main.lua
+++ client/main.lua
@@ -2,31 +2,37 @@ while not NetworkIsSessionStarted() do
     Wait(500)
 end
 
+local PlayerPedId, GetEntityCoords = PlayerPedId, GetEntityCoords
+
 local displayableObjects, maxDistance = {}, Convar:Get('draw-distance') --[[ @as number ]]
+local playerPed = PlayerPedId()
 
 CreateThread(function ()
     while true do
-        local playerPed = PlayerPedId()
-        local coords = GetEntityCoords(playerPed)    
+        playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
 
-        displayableObjects = Objects.cycle(function (obj)
+        displayableObjects = Objects.filter(function (obj)
             return obj:shouldRender(coords, maxDistance)
         end)
+
         Wait(1000)
     end
 end)
 
 CreateThread(function ()
     while true do
+        local coords
         if #displayableObjects == 0 then
             Wait(500)
         else
-            Wait(1)
-        end
+            coords = GetEntityCoords(playerPed)
+            Wait(0)
 
-        for i = 1, #displayableObjects do
-            local obj = displayableObjects[i]
-            obj:render()
+            for i = 1, #displayableObjects do
+                local obj = displayableObjects[i]
+                obj:render(coords, maxDistance)
+            end
         end
     end
 end)
